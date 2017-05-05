acad39045315cfb9736aa7b5f2754df59f411323
becrux <tony@becrux.com>
2017-05-05 22:03:45 +0200

    Added startup tests

--- tests/mgr/startup.cpp
+++ tests/mgr/startup.cpp
@@ -1,18 +1,99 @@
+#include <memory>
+
 #include "gtest/gtest.h"
 #include "win32/library.hpp"
-#include "util/methodscope.hpp"
+#include "common/version.hpp"
+
+#include "xfsapi.h"
 
 static Windows::Library *lib = nullptr;
 
-TEST(MgrLoader, NoParam)
-{ 
-  EXPECT_TRUE(true);
+TEST(MgrLoader, LibIsLoaded)
+{
+  EXPECT_NE(lib,nullptr);
 }
 
-int main(int argc, char **argv)
+TEST(MgrLoader, BadVersionRange)
 {
-  lib = new Windows::Library(L"msxfs.dll");
-  MethodScope libScope([] () { delete lib; });
+  XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,0));
+  WFSVERSION xfsV;
+
+  EXPECT_EQ(
+    lib->call< HRESULT >(
+      "WFSStartUp",
+      vr.value(),
+      &xfsV),
+    WFS_ERR_INTERNAL_ERROR);
+}
+
+TEST(MgrLoader, VersionTooHigh)
+{
+  XFS::VersionRange vr(XFS::Version(3,30),XFS::Version(3,40));
+  WFSVERSION xfsV;
+
+  EXPECT_EQ(
+    lib->call< HRESULT >(
+      "WFSStartUp",
+      vr.value(),
+      &xfsV),
+    WFS_ERR_API_VER_TOO_HIGH);
+}
+
+TEST(MgrLoader, VersionTooLow)
+{
+  XFS::VersionRange vr(XFS::Version(3,0),XFS::Version(3,10));
+  WFSVERSION xfsV;
+
+  EXPECT_EQ(
+    lib->call< HRESULT >(
+      "WFSStartUp",
+      vr.value(),
+      &xfsV),
+    WFS_ERR_API_VER_TOO_LOW);
+}
+
+TEST(MgrLoader, InvalidPointer)
+{
+  XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,20));
+
+  EXPECT_EQ(
+    lib->call< HRESULT >(
+      "WFSStartUp",
+      vr.value(),
+      NULL),
+    WFS_ERR_INVALID_POINTER);
+}
+
+TEST(MgrLoader, PlainCall)
+{
+  XFS::VersionRange vr(XFS::Version(3,0),XFS::Version(3,20));
+  WFSVERSION xfsV;
+
+  EXPECT_EQ(
+    lib->call< HRESULT >(
+      "WFSStartUp",
+      vr.value(),
+      &xfsV),
+    WFS_SUCCESS);
+}
+
+TEST(MgrLoader, AlreadyStarted)
+{
+  XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,20));
+  WFSVERSION xfsV;
+
+  EXPECT_EQ(
+    lib->call< HRESULT >(
+      "WFSStartUp",
+      vr.value(),
+      &xfsV),
+    WFS_ERR_ALREADY_STARTED);
+}
+
+int wmain(int argc, wchar_t **argv, wchar_t **envp)
+{
+  Windows::Library l{ std::wstring(argv[1]) };
+  lib = &l;
   
   ::testing::InitGoogleTest(&argc,argv);
 
