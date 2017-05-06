f6996d0c9f7bbfb48e575442678f4cc9c8fa77d7
becrux <tony@becrux.com>
2017-05-06 10:50:37 +0200

    Replaced GoogleTest with Catch

--- tests/mgr/startup.cpp
+++ tests/mgr/startup.cpp
@@ -1,6 +1,8 @@
 #include <memory>
 
-#include "gtest/gtest.h"
+#define CATCH_CONFIG_RUNNER
+#include "catch/catch.hpp"
+
 #include "win32/library.hpp"
 #include "common/version.hpp"
 
@@ -8,86 +10,83 @@
 
 static Windows::Library *lib = nullptr;
 
-TEST(MgrLoader, LibIsLoaded)
+TEST_CASE("StartUp", "[XFS Manager]")
 {
-  EXPECT_NE(lib,nullptr);
-}
-
-TEST(MgrLoader, BadVersionRange)
-{
-  XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,0));
-  WFSVERSION xfsV;
-
-  EXPECT_EQ(
-    lib->call< HRESULT >(
-      "WFSStartUp",
-      vr.value(),
-      &xfsV),
-    WFS_ERR_INTERNAL_ERROR);
-}
-
-TEST(MgrLoader, VersionTooHigh)
-{
-  XFS::VersionRange vr(XFS::Version(3,30),XFS::Version(3,40));
-  WFSVERSION xfsV;
-
-  EXPECT_EQ(
-    lib->call< HRESULT >(
-      "WFSStartUp",
-      vr.value(),
-      &xfsV),
-    WFS_ERR_API_VER_TOO_HIGH);
-}
-
-TEST(MgrLoader, VersionTooLow)
-{
-  XFS::VersionRange vr(XFS::Version(3,0),XFS::Version(3,10));
-  WFSVERSION xfsV;
-
-  EXPECT_EQ(
-    lib->call< HRESULT >(
-      "WFSStartUp",
-      vr.value(),
-      &xfsV),
-    WFS_ERR_API_VER_TOO_LOW);
-}
-
-TEST(MgrLoader, InvalidPointer)
-{
-  XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,20));
-
-  EXPECT_EQ(
-    lib->call< HRESULT >(
-      "WFSStartUp",
-      vr.value(),
-      NULL),
-    WFS_ERR_INVALID_POINTER);
-}
-
-TEST(MgrLoader, PlainCall)
-{
-  XFS::VersionRange vr(XFS::Version(3,0),XFS::Version(3,20));
-  WFSVERSION xfsV;
-
-  EXPECT_EQ(
-    lib->call< HRESULT >(
-      "WFSStartUp",
-      vr.value(),
-      &xfsV),
-    WFS_SUCCESS);
-}
-
-TEST(MgrLoader, AlreadyStarted)
-{
-  XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,20));
-  WFSVERSION xfsV;
-
-  EXPECT_EQ(
-    lib->call< HRESULT >(
-      "WFSStartUp",
-      vr.value(),
-      &xfsV),
-    WFS_ERR_ALREADY_STARTED);
+  SECTION("loading library")
+  {
+    REQUIRE(lib != nullptr);
+  }
+  
+  SECTION("passing bad version range")
+  {
+    XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,0));
+    WFSVERSION xfsV;
+  
+    REQUIRE(
+      lib->call< HRESULT >(
+        "WFSStartUp",
+        vr.value(),
+        &xfsV) == WFS_ERR_INTERNAL_ERROR);
+  }
+  
+  SECTION("passing too high version range")
+  {
+    XFS::VersionRange vr(XFS::Version(3,30),XFS::Version(3,40));
+    WFSVERSION xfsV;
+  
+    REQUIRE(
+      lib->call< HRESULT >(
+        "WFSStartUp",
+        vr.value(),
+        &xfsV) == WFS_ERR_API_VER_TOO_HIGH);
+  }
+  
+  SECTION("passing too low version range")
+  {
+    XFS::VersionRange vr(XFS::Version(3,0),XFS::Version(3,10));
+    WFSVERSION xfsV;
+  
+    REQUIRE(
+      lib->call< HRESULT >(
+        "WFSStartUp",
+        vr.value(),
+        &xfsV) == WFS_ERR_API_VER_TOO_LOW);
+  }
+  
+  SECTION("passing invalid output pointer")
+  {
+    XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,20));
+  
+    REQUIRE(
+      lib->call< HRESULT >(
+        "WFSStartUp",
+        vr.value(),
+        NULL) == WFS_ERR_INVALID_POINTER);
+  }
+  
+  SECTION("working call")
+  {
+    XFS::VersionRange vr(XFS::Version(3,0),XFS::Version(3,20));
+    WFSVERSION xfsV;
+  
+    REQUIRE(
+      lib->call< HRESULT >(
+        "WFSStartUp",
+        vr.value(),
+        &xfsV) == WFS_SUCCESS);
+  }
+  
+  SECTION("check if it has already started")
+  {
+    XFS::VersionRange vr(XFS::Version(3,20),XFS::Version(3,20));
+    WFSVERSION xfsV;
+  
+    REQUIRE(
+      lib->call< HRESULT >(
+        "WFSStartUp",
+        vr.value(),
+        &xfsV) == WFS_ERR_ALREADY_STARTED);
+  }
 }
 
 int wmain(int argc, wchar_t **argv, wchar_t **envp)
@@ -95,7 +94,8 @@ int wmain(int argc, wchar_t **argv, wchar_t **envp)
   Windows::Library l((std::wstring(argv[1])));
   lib = &l;
   
-  ::testing::InitGoogleTest(&argc,argv);
+  int catchArgc = 4;
+  char *catchArgv[] = { "", "-s", "-d", "yes" };
 
-  return RUN_ALL_TESTS();
+  return Catch::Session().run(catchArgc,catchArgv);
 }
