27acef46444d339de052ffecc330d56da688f5c5
becrux <tony@becrux.com>
2017-05-07 13:31:02 +0200

    Added missing file

---
+++ tests/catch.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef XFSPP_CATCH_HPP
+#define XFSPP_CATCH_HPP
+
+#ifdef _MSC_VER
+#pragma warning(push, 0)
+#endif
+
+#define CATCH_CONFIG_RUNNER
+#include "catch/catch.hpp"
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+#endif
